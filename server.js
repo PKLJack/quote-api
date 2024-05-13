8ae836850e577a52eeb99965a128bb06e569048c
PKLJack <59538991+PKLJack@users.noreply.github.com>
2024-05-14 00:05:40 +0100

    Extras

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+// const { quotes } = require('./data');
+const { quotes } = require('./data2');
+const {
+  getRandomElement,
+  getElementById,
+  getElementIndexById,
+} = require('./utils');
+
+let nextId = quotes.length + 1;
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 app.listen(PORT);
+
+// app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 const quotesRouter = express.Router();
 app.use('/api/quotes', quotesRouter);
@@ -35,7 +45,43 @@ quotesRouter.post('/', (req, res, next) => {
     return res.status(400).send();
   }
 
-  const quoteObj = { quote, person };
+  const quoteObj = { id: nextId, quote, person };
+  nextId++;
   quotes.push(quoteObj);
   return res.send({ quote: quoteObj });
+});
+
+quotesRouter.put('/:id', (req, res, next) => {
+  const id = Number(req.params.id);
+  console.log(req.body);
+
+  const { quote, person } = req.body;
+
+  if (!quote || !person) {
+    return res.status(400).send();
+  }
+
+  const quoteObj = getElementById(quotes, id);
+  if (!quoteObj) {
+    return res.status(404).send();
+  }
+
+  quoteObj.quote = quote;
+  quoteObj.person = person;
+
+  return res.status(200).send();
+});
+
+quotesRouter.delete('/:id', (req, res, next) => {
+  const id = Number(req.params.id);
+
+  const index = getElementIndexById(quotes, id);
+
+  if (index === -1) {
+    return res.status(404).send();
+  }
+
+  const quoteObj = quotes.splice(index, 1)[0];
+
+  return res.status(200).send({ quote: quoteObj });
 });
--- utils.js
+++ utils.js
@@ -1,8 +1,22 @@
-const getRandomElement = arr => {
+const getRandomElement = (arr) => {
   if (!Array.isArray(arr)) throw new Error('Expected an array');
   return arr[Math.floor(Math.random() * arr.length)];
-}
+};
+
+const getElementById = (arr, id) => {
+  if (!Array.isArray(arr)) throw new Error('Expected an array');
+
+  return arr.find((element) => element.id === id);
+};
+
+const getElementIndexById = (arr, id) => {
+  if (!Array.isArray(arr)) throw new Error('Expected an array');
+
+  return arr.findIndex((element) => element.id === id);
+};
 
 module.exports = {
-  getRandomElement
+  getRandomElement,
+  getElementById,
+  getElementIndexById,
 };
